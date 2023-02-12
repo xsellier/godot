acef49102546e25cc9c36c2eef618e4728b91e95
Andy Nichols <nezticle@gmail.com>
2023-02-12 13:10:09 +0100

    NX: Update export template script for RC

--- misc/scripts/export_nx_templates.py
+++ misc/scripts/export_nx_templates.py
@@ -4,7 +4,7 @@
 import os
 
 #version_string = version.major + '.' + version.minor + '.' + version.patch + '.' + version.status
-version_string = '4.0.beta'
+version_string = '4.0.rc'
 appdata_dir = os.getenv('APPDATA')
 target_dir = appdata_dir + '\\Godot\\export_templates\\' + version_string + '\\'
 
